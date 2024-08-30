383aaa5331660710142851330945c53efe44d179
giuseppe <capassog97@gmail.com>
2024-08-30 19:26:00 +0200

    Refactored colors in own dir

---
+++ colors/tokyo.h
@@ -0,0 +1,20 @@
+/* tokyo-night color scheme */
+static const char col_normfg[]      = "#f8f8f2";
+static const char col_normbg[]      = "#1a1b26";
+static const char col_normborder[]  = "#1a1b26";
+static const char col_selfg[]       = "#1a1b26";
+static const char col_selbg[]       = "#7aa2f7";
+static const char col_selborder[]   = "#aaaaaa";
+/* tag rainbow */
+static const char col_gray[]        = "#414868";
+static const char col_red[]         = "#c53b53";
+static const char col_orange[]      = "#ff9e64";
+static const char col_yellow[]      = "#ffc777";
+static const char col_green[]       = "#c3e88d";
+static const char col_cyan[]        = "#545c7e";
+static const char col_blue[]        = "#7aa2f7";
+static const char col_purple[]      = "#3d59a1";
+static const char col_pink[]        = "#FDB0C0";
+static const char col_white[]       = "#b4f9f8";
+/* other */
+static const char col_borderbar[]   = "#000000";
